95f9981fef569cc55843773db11f8687c4cdb708
Jonathan <jconwell3115@gmail.com>
2025-02-23 21:29:44 -0500

    Corrected the variable to store the lines from the read in config file

--- golden_config_generator.py
+++ golden_config_generator.py
@@ -89,8 +89,8 @@ class ConfigGenerator:
         self.project_path = r'Place_Holder for now'
         self.template_path = os.path.join(self.project_path, r'Templates')
         self.old_config_file = ''
-        self.old_config = []
-        self.old_config_var = ''
+        self.old_config = ''
+        self.old_config_list = []
         self.switch_template = os.path.join(self.template_path,  r'Switch_template.j2')
         self.new_config = os.path.join(self.project_path, r'Configurations\New')  # Set the path for new config
         self.new_config_template = ''
@@ -114,13 +114,11 @@ class ConfigGenerator:
         =========
         Variables
         =========
-        `
-        ``:var lst dict_list:`` The list of all the dictionaries to use to update the ``self.parameters_dict`` for ease
-        of rendering the jinja2 template\n
+
 
         :raise FileNotFoundError: If the filename is incorrect or file not being present in the correct folder.
         :except PermissionError: If the file is open
-
+        :return: self.old_config
         :returns: **[dict, list, str]** Several different data stores with dictionaries, lists and strings of configuration
         extracted from the old switch configuration
         :rtype: dict
@@ -138,7 +136,7 @@ class ConfigGenerator:
         time.sleep(.2)
         try:
             with open(self.old_config, 'r', encoding='UTF-8') as old_config:
-                self.old_config = old_config.readlines()
+                self.old_config_list = old_config.readlines()
         except FileNotFoundError:
             print('\n' + self.old_config, 'is not a valid file\nPlease check the filename and try again.\n')
             sys.exit()
@@ -154,6 +152,9 @@ class ConfigGenerator:
 
         Example: S1-EN-3320-104-1
 
+        =========
+        Variables
+        =========
         ``:var dict hostname_dict:`` The hostname of the switch\n
         ``:var dict location_dict:`` The building and room number for SNMP lookup\n
         ``:var dict site_dict:`` The site parameter used for conditions in site specific template selection\n
@@ -170,7 +171,7 @@ class ConfigGenerator:
         access_switch_prefix_list = ['AS', 'SE', 'EN']
         condition_dict_list = [self.site_dict, self.switch_type_dict]
 
-        for line in self.old_config:
+        for line in self.old_config_list:
             hostname_list = line.split(' ')  # Create a new list split on blank spaces
             hostname_dict['hostname'] = hostname_list[1].replace('\n', '').upper()
             site_prefix = hostname_dict['hostname'][:2]  # Get the prefix from the hostname
@@ -204,14 +205,14 @@ class ConfigGenerator:
         """
         vlan_dict = {'vlans': {}}
 
-        for line in self.old_config:
+        for line in self.old_config_list:
             if line.startswith('spanning-tree vlan'):  # Get spanning-tree vlan priorities if they exist
                 self.vlan_priority = line
             elif line.startswith('vlan'):  # Get VLAN database information
                 vlan_list = line.split(' ')
                 vlan_id = vlan_list[1].replace('\n', '')
                 vlan_dict['vlans'].setdefault(vlan_id, {})
-                for vlan in self.old_config:
+                for vlan in self.old_config_list:
                     if vlan.startswith(' name'):
                         vlan_name_list = vlan.split(' ')
                         vlan_dict['vlans'][vlan_id]['name'] = vlan_name_list[-1].replace('\n', '')
@@ -224,11 +225,11 @@ class ConfigGenerator:
 
         :return:
         """
-        for line in self.old_config:
+        for line in self.old_config_list:
             if line.startswith('interface'):  # Copy the interface configurations
                 interfaces = ''
                 interfaces += line  # First Line is the interface name
-                for interface in self.old_config:
+                for interface in self.old_config_list:
                     if '!' in interface:  # Stop copying lines at the !
                         interfaces += '!\n'
                         break
@@ -246,10 +247,10 @@ class ConfigGenerator:
 
         :return:
         """
-        for line in self.old_config:
+        for line in self.old_config_list:
             if line.startswith('router '):  # Copy all router instances as a block config
                 self.router_config += line
-                for router_config in self.old_config:
+                for router_config in self.old_config_list:
                     if '!' in router_config:
                         self.router_config += '!'
                         break
@@ -269,14 +270,12 @@ class ConfigGenerator:
         source_interface_dict = {'source_interface': ''}
         mtu_dict = {'mtu': ''}
         gateway_dict = {'gateway': ''}
-        for line in self.old_config:
-
+        for line in self.old_config_list:
             if line.startswith('logging'):  # Copy the logging information as a block config
                 if 'buffered' in line:  # Skip buffered logging config, this will be set by a new standard
                     continue
                 else:
                     self.logging += line
-
             # Gather the source interface from the 'tacacs source-interface' command
             elif line.startswith('ip tacacs source-interface'):
                 source_list = line.split(' ')
